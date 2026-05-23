942c14713b3bd538c4bf3cb7052a47b51146ca34
boaat <36909227+boaaaat@users.noreply.github.com>
2026-05-22 19:10:34 -0700

    new test dataset

--- action_space.py
+++ action_space.py
@@ -2,14 +2,15 @@ import os
 from typing import Dict, List, Optional
 
 
-selected_game = "greenville"
+selected_game = "greenville_test"
 
 key_names: Dict[str, List[str]] = {
     "doors": ["w", "a", "s", "d", "e", "1", "2", "3", "Key.shift", "Key.space"],
     "flee_the_facility": ["w", "a", "s", "d", "e", "Key.space", "Key.shift"],
     "lol": ["q", "w", "e", "r", "a", "f", "d", "c", "v", "b", "p", "x", "1", "2", "3", "4", "5", "Key.space", "Key.ctrl_l", "Key.tab"],
     "arc_raiders": ["w", "a", "s", "d", "e", "f", "h", "q", "v", "r", "x", "m", "g", "1", "2", "3", "Key.space", "Key.shift", "Key.ctrl_l", "Key.tab", "Key.alt_l"], #, "Key.esc"]
-    "greenville": ['w', 'a', 's', 'd', 'e', 'q', 'c', 'z']
+    "greenville": ['w', 'a', 's', 'd', 'e', 'q', 'c', 'z'],
+    "greenville_test": ['w', 'a', 's', 'd', 'e', 'q', 'c', 'z']
 }
 
 mouse_button_names: Dict[str, List[str]] = {
@@ -17,7 +18,8 @@ mouse_button_names: Dict[str, List[str]] = {
     "flee_the_facility": ["left_click", "right_click"],
     "lol": ["left_click", "right_click"],
     "arc_raiders": ["left_click", "right_click", "middle_click", "scroll_up", "scroll_down"],
-    'greenville': []
+    'greenville': [],
+    'greenville_test': []
 }
 
 
